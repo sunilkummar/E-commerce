57cb1f1ecf15226fad476f06cb5c48481dccd7ab
sunilkummar <sunilkumar9911686682@gmail.com>
2016-07-09 11:53:12 +0530

    1st

---
+++ home.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<link href="mystyle.css" rel="stylesheet" type="text/css"/>
+</head>
+<body bgcolor= 'sky blue'>
+<div id="wrap">
+<?php include('head.php');
+?>
+<div id="pic">
+<img src ="q.jpg" width=600px height=380px>
+</div>
+</div>
+</body>
+</html>
